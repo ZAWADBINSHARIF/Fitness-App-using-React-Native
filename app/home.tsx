5e15f2e0741a1738de9abbbbf9d425ea52e8074d
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2024-08-22 15:36:51 +0600

    Nativewind has been removed

--- app/home.tsx
+++ app/home.tsx
@@ -1,18 +1,102 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { useNavigation } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+import { Colors } from '@/constants/Colors';
 
 const home = () => {
 
     const router = useRoute();
+    const [notification, setNotification] = useState(false);
 
     console.log(router.name);
 
     return (
-        <View>
-            <Text>home</Text>
-        </View>
+        <SafeAreaView>
+            <View
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    gap: 12,
+                    paddingHorizontal: 12,
+                    backgroundColor: 'white'
+                }}
+            >
+
+                {/* Punchiling and Avatar  */}
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        paddingTop: 12,
+                    }}
+                >
+                    <View>
+                        <Text
+                            style={{
+                                fontSize: 48,
+                                fontWeight: 700
+                            }}
+                        >Ready To</Text>
+                        <Text
+                            style={{
+                                fontSize: 48,
+                                fontWeight: 700,
+                                color: Colors.rose
+                            }}
+                        >WorkOut</Text>
+                    </View>
+
+                    <View
+                        style={{
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            gap: 12
+                        }}
+                    >
+                        <Image
+                            source={require("@/assets/images/avatar.png")}
+                            style={{
+                                width: 64,
+                                height: 64,
+                                borderRadius: 32
+                            }}
+                        />
+
+                        <View
+                            style={{
+                                width: 48,
+                                height: 48,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderRadius: 24,
+                                backgroundColor: 'rgb(226 232 240)',
+                                borderColor: 'gray'
+                            }}
+                        >
+                            <FontAwesome
+                                name="bell"
+                                size={32}
+                                color={notification ? 'rgb(190 18 60)' : 'gray'}
+                                onPress={() => setNotification(!notification)}
+                            />
+                        </View>
+
+                    </View>
+
+                </View>
+
+                {/* Image Slider  */}
+                <View>
+
+                </View>
+
+            </View>
+        </SafeAreaView >
     );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -8,35 +8,92 @@ import Animated, { FadeInDown } from 'react-native-reanimated';
 
 const index = () => {
     return (
-        <View className="flex-1 flex justify-end">
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'flex-end'
+            }}>
             <StatusBar style='light' />
             <Image
-                className='h-full w-full absolute'
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    position: 'absolute'
+                }}
                 source={require("@/assets/images/welcome4.jpg")}
             />
 
             <LinearGradient
                 colors={['transparent', '#18181b']}
-                style={{ width: "100%", height: "100%" }}
                 start={{ x: 0.5, y: 0 }}
                 end={{ x: 0.5, y: 0.9 }}
-                className='flex justify-end pb-12 space-y-8'
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    flex: 1,
+                    justifyContent: 'flex-end',
+                    paddingBottom: 48,
+                    gap: 28,
+                }}
             >
-                <Animated.View entering={FadeInDown.delay(100).springify()} className='flex items-center'>
-                    <Text className='text-4xl text-white font-bold tracking-wide'>
-                        Best <Text className='text-rose-500'>Workouts</Text>
+                <Animated.View entering={FadeInDown.delay(100).springify()}
+                    style={{
+                        alignItems: 'center'
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: "white",
+                            fontSize: 40,
+                            fontWeight: 'bold'
+                        }}
+                    >
+                        Best <Text
+                            style={{
+                                color: "rgb(244 63 94)",
+                                fontSize: 40,
+                                fontWeight: 'bold'
+                            }}
+                        >Workouts</Text>
                     </Text>
-                    <Text className='text-4xl text-white font-bold tracking-wide'>
+                    <Text
+                        style={{
+                            fontSize: 40,
+                            color: 'white',
+                            fontWeight: 'bold',
+                        }}
+                    >
                         For you
                     </Text>
                 </Animated.View>
 
-                <Animated.View entering={FadeInDown.delay(200).springify()}>
+                <Animated.View entering={FadeInDown.delay(200).springify()}
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
                     <TouchableOpacity
-                        className='p-3 w-[80%] flex items-center justify-center mx-auto border-2 border-neutral-200 bg-rose-500 rounded-full'
+                        style={{
+                            padding: 12,
+                            width: '80%',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderWidth: 2,
+                            borderRadius: 20,
+                            borderColor: "rgb(229 229 229)",
+                            backgroundColor: "rgb(244 63 94)"
+                        }}
                         onPress={() => router.push("/home")}
                     >
-                        <Text className='text-center text-white text-3xl font-bold tracking-widest'>
+                        <Text
+                            style={{
+                                textAlign: 'center',
+                                fontSize: 32,
+                                color: 'white',
+                                fontWeight: 'bold',
+                            }}
+                        >
                             Get Started
                         </Text>
                     </TouchableOpacity>
